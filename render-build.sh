043f9f10cffe96b97b38f3696bd31b3c88802d98
ashraful2871 <ashrafulislam2758@gmail.com>
2025-12-05 22:29:48 +0600

    fixed command

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bun install
-bun run build
-bunx prisma generate
-bunx prisma migrate deploy
+npm install
+npm run build
+npx prisma generate
+npx prisma migrate deploy
