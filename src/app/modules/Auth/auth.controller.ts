c7b433c2b1d6a6cdba7ccb23b3da7aa84bbdd7e0
ashraful2871 <ashrafulislam2758@gmail.com>
2025-12-21 21:31:50 +0600

    added refresh token api

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { authServices } from "./auth.service";
 import { Request, Response } from "express";
+import config from "../../../config";
 
 //user registration controller
 const userRegistration = catchAsync(async (req: Request, res: Response) => {
@@ -66,9 +67,84 @@ const getMe = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+  const { refreshToken } = req.cookies;
+
+  const accessTokenExpiresIn = config.jwt.expires_in as string;
+  const refreshTokenExpiresIn = config.jwt.refresh_token_expires_in as string;
+
+  let accessTokenMaxAge = 0;
+  const accessTokenUnit = accessTokenExpiresIn.slice(-1);
+  const accessTokenValue = parseInt(accessTokenExpiresIn.slice(0, -1));
+  if (accessTokenUnit === "y") {
+    accessTokenMaxAge = accessTokenValue * 365 * 24 * 60 * 60 * 1000;
+  } else if (accessTokenUnit === "M") {
+    accessTokenMaxAge = accessTokenValue * 30 * 24 * 60 * 60 * 1000;
+  } else if (accessTokenUnit === "w") {
+    accessTokenMaxAge = accessTokenValue * 7 * 24 * 60 * 60 * 1000;
+  } else if (accessTokenUnit === "d") {
+    accessTokenMaxAge = accessTokenValue * 24 * 60 * 60 * 1000;
+  } else if (accessTokenUnit === "h") {
+    accessTokenMaxAge = accessTokenValue * 60 * 60 * 1000;
+  } else if (accessTokenUnit === "m") {
+    accessTokenMaxAge = accessTokenValue * 60 * 1000;
+  } else if (accessTokenUnit === "s") {
+    accessTokenMaxAge = accessTokenValue * 1000;
+  } else {
+    accessTokenMaxAge = 1000 * 60 * 60; // default 1 hour
+  }
+
+  // convert refreshTokenExpiresIn to milliseconds
+  let refreshTokenMaxAge = 0;
+  const refreshTokenUnit = refreshTokenExpiresIn.slice(-1);
+  const refreshTokenValue = parseInt(refreshTokenExpiresIn.slice(0, -1));
+  if (refreshTokenUnit === "y") {
+    refreshTokenMaxAge = refreshTokenValue * 365 * 24 * 60 * 60 * 1000;
+  } else if (refreshTokenUnit === "M") {
+    refreshTokenMaxAge = refreshTokenValue * 30 * 24 * 60 * 60 * 1000;
+  } else if (refreshTokenUnit === "w") {
+    refreshTokenMaxAge = refreshTokenValue * 7 * 24 * 60 * 60 * 1000;
+  } else if (refreshTokenUnit === "d") {
+    refreshTokenMaxAge = refreshTokenValue * 24 * 60 * 60 * 1000;
+  } else if (refreshTokenUnit === "h") {
+    refreshTokenMaxAge = refreshTokenValue * 60 * 60 * 1000;
+  } else if (refreshTokenUnit === "m") {
+    refreshTokenMaxAge = refreshTokenValue * 60 * 1000;
+  } else if (refreshTokenUnit === "s") {
+    refreshTokenMaxAge = refreshTokenValue * 1000;
+  } else {
+    refreshTokenMaxAge = 1000 * 60 * 60 * 24 * 30; // default 30 days
+  }
+
+  const result = await authServices.refreshToken(refreshToken);
+  res.cookie("accessToken", result.accessToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    maxAge: accessTokenMaxAge,
+  });
+
+  res.cookie("refreshToken", result.refreshToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    maxAge: refreshTokenMaxAge,
+  });
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Access token genereated successfully!",
+    data: {
+      message: "Access token genereated successfully!",
+    },
+  });
+});
+
 export const authController = {
   userRegistration,
   login,
   changePassword,
   getMe,
+  refreshToken,
 };
